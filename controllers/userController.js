41fc1b5c092efc6f1a285aec36227a4d0d2c9788
Abdelrahman Deghedy <abdelrahman.deghedy@gmail.com>
2023-01-30 14:02:47 +0200

    Fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,20 +3,11 @@ const bot = require("../services/createBot");
 
 exports.getRegisteredUsers = async (req, res) => {
   const notifiedUsers = await NotificationUsers.find({});
-  let registeredUsernames = "";
-
-  notifiedUsers.forEach((user) => {
-    registeredUsernames += user.username;
-    registeredUsernames += ", ";
-  });
-
-  bot.newBot.sendMessage("Registered Users are:");
-  bot.newBot.sendMessage(registeredUsernames);
 
   res.status(201).json({
     status: "success",
     data: {
-      registeredUsernames,
+      notifiedUsers,
     },
   });
 };
